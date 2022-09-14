aac0ad723625aa3ed99477d0c83c4cbe05bbb44a
Omar Morales <omarmv101@yahoo.com>
2022-09-13 20:55:21 -0700

    Deployed website

--- src/index.js
+++ src/index.js
@@ -11,8 +11,7 @@ root.render(
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
